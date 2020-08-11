bca55277195d1c97022b399606c2fb5b67c0a221
Siddharth Tumre <siddharthtumre2@gmail.com>
2020-08-11 16:09:22 +0530

    React Router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
-import { Container, Navbar, Jumbotron, Row } from "react-bootstrap";
+import { Container, Navbar, Jumbotron, Row, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
   render() {
     return (
       <>
-        <Navbar variant="dark">
+        <Navbar collapseOnSelect variant="dark" expand="md">
           <Container>
-            <Navbar.Brand href="/">Ristorante Con Fusion</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Brand className="mr-auto" href="/">
+              <img
+                src="assets/images/logo.png"
+                height="30"
+                width="41"
+                alt="Ristorante Con Fusion"
+              />
+            </Navbar.Brand>
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav>
+                <Nav.Item>
+                  <NavLink className="nav-link" to="/home">
+                    <span className="fa fa-home fa-lg"></span> Home
+                  </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <span className="fa fa-info fa-lg"></span> About Us
+                  </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavLink className="nav-link" to="/menu">
+                    <span className="fa fa-list fa-lg"></span> Menu
+                  </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavLink className="nav-link" to="/contactus">
+                    <span className="fa fa-address-card fa-lg"></span> Contact
+                    Us
+                  </NavLink>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
         <Jumbotron>
---
+++ src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Home = () => {
+  return (
+    <Container>
+      <h4>Home</h4>
+    </Container>
+  );
+};
+
+export default Home;
